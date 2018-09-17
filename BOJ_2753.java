fd45bf599517cf64398567b55dae32ab9cfb29b1
QuatreMains <plutokor@gmail.com>
2018-09-17 19:36:46 +0900

    solve 2753

---
+++ BOJ_2753.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] argv) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%4==0)
+        {
+            if (n%100!=0||n%400==0)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+        else
+            System.out.println("0");
+    }
+}
