bc34d2d6ec6613c81a5d4556323c437ae6c030b6
QuatreMains <plutokor@gmail.com>
2018-09-20 15:18:04 +0900

    solve 2566

---
+++ BOJ_2566.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String argv[]) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[9][9];
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++)
+            {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int max = arr[0][0];
+        int x=0,y=0;
+        for(int i=1; i<9; i++){
+            for(int j=0; j<9; j++)
+            {
+                if(arr[i][j]>max){
+                    max = arr[i][j];
+                    x = i+1;
+                    y = j+1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.print(x+" "+y);
+    }
+}
