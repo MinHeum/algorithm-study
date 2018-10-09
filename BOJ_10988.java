b1da857b0a043195c50aa38cd6ca0f2f53e854a4
QuatreMains <plutokor@gmail.com>
2018-10-10 03:00:28 +0900

    Solve 10988

---
+++ BOJ_10988.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        boolean is_palindrom = true;
+        for(int i=0; i<(s.length()/2)+1; i++){
+            if(s.charAt(i)!=s.charAt(s.length()-(i+1)))
+                is_palindrom = false;
+        }
+        System.out.println(is_palindrom?"1":"0");
+    }
+}
