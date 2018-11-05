cfd290e16743d966d5aff3aa12310746c00ddedf
QuatreMains <plutokor@gmail.com>
2018-11-05 19:11:13 +0900

    Solve 6376

--- BOJ_6376.java
+++ BOJ_6376.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String args[]){
-        int n=0;
         System.out.println("n e");
         System.out.println("- -----------");
         double sig = 0;
