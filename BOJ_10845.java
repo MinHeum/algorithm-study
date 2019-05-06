565201f270ccbdf9b20aac051af1a88a15e20655
QuatreMains <plutokor@gmail.com>
2019-05-06 18:52:21 +0900

    solve 10845

---
+++ BOJ_10845.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Main{
+    public static void main(String asgs[]){
+        LinkedList<Integer> que = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i<n; i++){
+            String s = sc.nextLine();
+            if (s.split(" ").length == 2){
+                que.addLast(Integer.valueOf(s.split(" ")[1]));
+            }
+            else{
+                if (s.contains("front")){
+                    if (que.isEmpty())
+                        System.out.println("-1");
+                    else
+                        System.out.println(que.getFirst());
+                }
+                else if (s.contains("back")){
+                    if (que.isEmpty())
+                        System.out.println("-1");
+                    else
+                        System.out.println(que.getLast());
+                }
+                else if (s.contains("size")){
+                    System.out.println(que.size());
+                }
+                else if (s.contains("empty")){
+                    System.out.println(que.isEmpty()?"1":"0");
+                }
+                else if (s.contains("pop")){
+                    if (que.isEmpty())
+                        System.out.println("-1");
+                    else{
+                        System.out.println(que.getFirst());
+                        que.removeFirst();
+                    }
+                }
+                else {
+                    System.out.println("Something is wrong");
+                }
+            }
+        }
+    }
+}
