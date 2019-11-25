fc2481ca6c2329d593f69414bd91afeb83bf6e24
QuatreMains <plutokor@gmail.com>
2019-11-25 11:22:46 +0900

    Solved 2884

---
+++ BOJ_2884.java
@@ -0,0 +1,21 @@
+// BOJ_2884.java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        int hour = Integer.parseInt(input.split(" ")[0]);
+        int minute = Integer.parseInt(input.split(" ")[1]);
+
+        int times = hour * 60 + minute;
+        times -= 45;
+        if (times < 0) times += 1440;
+
+        int hourNew = times / 60;
+        int minuteNew = times % 60;
+
+        System.out.println(hourNew+" "+minuteNew);
+    }
+}
