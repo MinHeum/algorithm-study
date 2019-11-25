8f4cb2465117aa58a06caa9951ed6bf3a9ceff63
QuatreMains <plutokor@gmail.com>
2019-11-25 11:09:38 +0900

    solved 1330

---
+++ BOJ_1330.java
@@ -0,0 +1,16 @@
+// BOJ_1330.java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        int a = Integer.parseInt(input.split(" ")[0]);
+        int b = Integer.parseInt(input.split(" ")[1]);
+
+        if (a>b) System.out.println('>');
+        else if (a<b) System.out.println('<');
+        else System.out.println("==");
+    }
+}
