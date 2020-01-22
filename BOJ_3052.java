08e10a0b7895c2e195d7957b839586175eecd2e5
QuatreMains <plutokor@gmail.com>
2020-01-22 15:03:10 +0900

    Solved 3052

---
+++ BOJ_3052.java
@@ -0,0 +1,25 @@
+// BOJ_3052.java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] lst = new int[10];
+        for (int i = 0; i < 10; i++) {
+            lst[i] = sc.nextInt();
+        }
+        int[] modLst = new int[10];
+        for (int i = 0; i < 10; i++) {
+            modLst[i] = lst[i] % 42;
+        }
+        int[] countLst = new int[42];
+        for (int i = 0; i < 10; i++) {
+            countLst[modLst[i]]++;
+        }
+        int count = 0;
+        for (int i = 0; i < 42; i++) {
+            if(countLst[i] > 0) count++;
+        }
+        System.out.println(count);
+    }
+}
