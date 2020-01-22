a2942ba7fd7aa2c179caf04eefa46d33aeea8f7b
QuatreMains <plutokor@gmail.com>
2020-01-22 15:09:13 +0900

    Solved 15596

---
+++ BOJ_15596.java
@@ -0,0 +1,11 @@
+// BOJ_15596.java
+public class Test {
+    private static long sum(int[] a) {
+        long result = 0;
+        for (int x: a
+        ) {
+            result += x;
+        }
+        return result;
+    }
+}
