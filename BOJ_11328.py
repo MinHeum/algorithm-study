0c3c379521460ff71a424559ff26ea836ae6885c
QuatreMains <plutokor@gmail.com>
2021-04-19 00:04:39 +0900

    Solved 11328

---
+++ BOJ_11328.py
@@ -0,0 +1,22 @@
+T = int(input())
+for _ in range(T):
+    arr1 = [0] * 26
+    arr2 = [0] * 26
+    word_a, word_b = map(str, input().split())
+    for i in word_a:
+        arr1[ord(i)-97] += 1
+    
+    for i in word_b:
+        arr2[ord(i)-97] += 1
+    
+    is_anagrams = True
+
+    for i in range(0,26):
+        if arr1[i] != arr2[i]:
+            is_anagrams = False
+            break
+    
+    if is_anagrams:
+        print("Possible")
+    else:
+        print("Impossible")
