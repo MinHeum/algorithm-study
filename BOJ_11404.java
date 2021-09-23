bb2ba7f69ffba6f4ee52fe8e30ca7d023f2e1155
MinHeum <parkminheum@ks.ac.kr>
2021-09-23 20:59:29 +0900

    Solved 11404

---
+++ BOJ_11404.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11404 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(map[i], Integer.MAX_VALUE >> 1);
+            map[i][i] = 0;
+        }
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+            map[a][b] = Math.min(c, map[a][b]);
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(map[i][j]);
+                if (j != n-1) {
+                    sb.append(' ');
+                }else {
+                    sb.append('\n');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
