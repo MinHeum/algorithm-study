74a0449028655ba67dcc1500e1882e8bc4191a7f
MinHeum <plutokor@gmail.com>
2023-09-24 03:42:48 +0900

    Solve BOJ_2504

--- BOJ/BOJ_2504.py
+++ BOJ/BOJ_2504.py
@@ -5,14 +5,16 @@ tmp = 1
 res = 0
 
 for i in range(len(s)):
-    this = s[i]   
+    this = s[i]
+    # '(' 일 때, tmp *= 2, stack.append(this)
     if this == '(':
         tmp *= 2
         stack.append(this)
+    # '[' 일 때, tmp *= 3, stack.append(this)
     elif this == '[':
         tmp *= 3
         stack.append(this)
-
+    # ')' 일 때, stack가 비어있거나 stack[-1]이 '['이면 res = 0, break
     elif this == ')':
         if not stack or stack[-1] == '[':
             res = 0
@@ -20,7 +22,8 @@ for i in range(len(s)):
         if s[i-1] == '(':
             res += tmp
         tmp //= 2
-        stack.pop()  
+        stack.pop()
+    # ']' 일 때, stack가 비어있거나 stack[-1]이 '('이면 res = 0, break
     else:
         if not stack or stack[-1] == '(':
             res = 0
@@ -28,8 +31,9 @@ for i in range(len(s)):
         if s[i-1] == '[':
             res += tmp
         tmp //= 3
-        stack.pop() 
+        stack.pop()
 
+# stack에 문자가 남아있으면 res = 0
 if stack:
     res = 0
 print(res)
