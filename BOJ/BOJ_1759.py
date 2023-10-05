621e3fe42cfa90886bc534f3d542763a19cbc25e
MinHeum <plutokor@gmail.com>
2023-10-06 01:06:34 +0900

    Solve BOJ 1759

--- BOJ/BOJ_1759.py
+++ BOJ/BOJ_1759.py
@@ -1,25 +1,22 @@
-from sys import stdin
-import itertools
+from itertools import combinations
 
-L, C = map(int,stdin.readline().split())
-lst = list(stdin.readline().split())
+L, C = map(int, input().split())
+lst = input().split()
 
-# 최소 한개의 모음 (a,e,i,o,u) 와 최소 두 개의 자음으로 구성
-lst_vowel = []
-lst_consonant = []
-for i in lst:
-    if i in ['a','e','i','o','u']:
-        lst_vowel.append(i)
-    else:
-        lst_consonant.append(i)
+vowels = ["a", "e", "i", "o", "u"]
+lst_vowel = [i for i in lst if i in vowels]
+lst_consonant = [i for i in lst if i not in vowels]
 
 answer = []
-for i in range(1, len(lst_vowel)+1):
-    for vowel in itertools.combinations(lst_vowel, i):
-        for consonant in itertools.combinations(lst_consonant, L-len(vowel)):
-            if len(consonant) >= 2: 
-                s = sorted(''.join(vowel+consonant))
-                answer.append(s)
+
+# 최소 한 개의 모음과 최소 두 개의 자음으로 구성된 조합을 생성
+for i in range(1, L - 1):
+    for vowel_comb in combinations(lst_vowel, i):
+        consonant_comb = combinations(lst_consonant, L - i)
+        for consonant in consonant_comb:
+            s = "".join(sorted(vowel_comb + consonant))
+            answer.append(s)
+
 answer.sort()
 for e in answer:
-    print(''.join(e))
+    print(e)
